1cb246080dfb0af2ae4012f3f2dfd0dfc59fc9ff
mohammedyaseen2211 <ysn6040@gmail.com>
2023-08-22 22:45:39 +0530

    classes and prototypes in js

---
+++ Js/Classes and prototypes/script.js
@@ -0,0 +1,23 @@
+
+const user1 = {
+    name:"chess",
+    score:3,
+    increment: function(){user1.score++;},
+}
+
+console.log( user1.increment());
+
+//can functionality on the pertinenet data
+
+
+// ---creting user3
+
+const user2 = {}
+user2.name = "carroms";
+user2.price =100;
+
+user2.increment = function da3(){
+    user2.price=60
+}
+
+console.log(user2.increment.da3())
